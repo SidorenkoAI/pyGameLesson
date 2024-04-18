7e60ab6ce2e2a8857fd5133151ec7f5a3107869f
sai <sai@mail.ru>
2024-04-18 20:29:33 +0700

    Коллизии с колобком

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from Kolobok import Kolobok
 from Chel import Chel
 
 from Asteroid import Asteroid
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = pygame.image.load('img/kolobok.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(0, 300)
+        self.rect.y = random.randint(0, 300)
+    def update(self):
+        self.rect.x += 1
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+
 def chel():
     pygame.init()
     screen = pygame.display.set_mode((2000, 1200))
@@ -12,9 +26,10 @@ def chel():
     pygame.display.set_caption('Chel')
     chel = Chel(path='img/chel.png', screen=screen)
     clock = pygame.time.Clock()
-
+    kolGroup = pygame.sprite.Group()
+    for i in range(10):
+        kolGroup.add(Mob())
     asteroids = Asteroid(path='img/asteroid.png', screen=screen)
-    last_update = pygame.time.get_ticks()
     while True:
         screen.fill((24,113,147))
         for event in pygame.event.get():
@@ -32,8 +47,13 @@ def chel():
 
         elif keys[pygame.K_RIGHT]:
             chel.update('right')
-
+        kolGroup.update()
+        kolGroup.draw(screen)
         chel.draw()
+        s = pygame.sprite.spritecollideany(chel, kolGroup)
+        if s:
+            kolGroup.remove(s)
+
         asteroids.update()
         pygame.display.flip()
     clock.tick(60)
