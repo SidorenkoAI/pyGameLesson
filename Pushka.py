f0046ac74772456dac7b6e2075430d592f2ba960
sai <sai@mail.ru>
2024-05-02 20:47:23 +0700

    Добавлено управление скоростью вылета ядра + анимация

--- Pushka.py
+++ Pushka.py
@@ -37,3 +37,10 @@ class Pushka(pygame.sprite.Sprite):
         self.rect.center = old_center
     def draw(self):
         self.screen.blit(self.image, self.rect)
+        r = self.speed * 1.7
+        g = 255 - self.speed
+        b = 0
+        color = (r,g,b)
+        pygame.draw.rect(surface=self.screen,
+                         rect=pygame.Rect(self.rect.x, self.rect.y - 50, self.speed, 20),
+                         color=color)
